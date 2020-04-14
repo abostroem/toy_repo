800848e134d7aeeb1c420162ec7e56a5bee00328
Azalee Bostroem <abostroem@gmail.com>
2020-04-14 10:18:21 -0700

    Playing around with VS Code features

--- code/hello_world.py
+++ code/hello_world.py
@@ -2,3 +2,13 @@ print("hello world")
 print("line 2")
 
 print("adding a line")
+
+my_new_variable = 3
+# np.mean()
+import numpy as np
+
+np.mean([1, 1, 1])
+
+
+def new_function(wrong="try me"):
+    pass
